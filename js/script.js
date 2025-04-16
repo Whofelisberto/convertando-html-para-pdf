3be11f500cdbddb1c27c4afd2bfba6d6d49f9ea4
Whofelisberto <felisberto563@gmail.com>
2025-04-16 15:47:57 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function downloadPdf() {
+  const item = document.querySelector(".content");
+
+  var opções = {
+     margin:   1,
+     filename: 'noticia-anime.pdf',
+     html2canvas: {scale: 2},
+     jsPDF: {unit: 'in' , format: 'a4', orientation: 'p' }
+  };
+  html2pdf().set(opções).from(item).save();
+}
